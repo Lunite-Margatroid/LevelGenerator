3e69c6bb53d5ba33e22786d4e6900548e12d417f
Lunite_Margatroid <534830021@qq.com>
2025-04-06 21:28:29 +0800

    [add] 添加测试用res

--- Cellular/src/CellularAutoma.cpp
+++ Cellular/src/CellularAutoma.cpp
@@ -2,8 +2,16 @@
 
 
 
+void CellularAutoma::Step()
+{
+}
+
 CellularAutoma::CellularAutoma(const sgm::Graph_boost<ggl::Graph>& gi)
 :m_GI(gi)
+{
+}
+
+void CellularAutoma::Generate(const std::string& beginLabel)
 {
 }
 
--- Cellular/src/CellularAutoma.h
+++ Cellular/src/CellularAutoma.h
@@ -6,10 +6,23 @@
 class CellularAutoma
 {
 private:
+	struct NodeInfo {
+		int index;					// 该节点在ggl::Graph中的index
+		int postIndex;				// 前置节点的index
+		int depth;					// 该节点相对于起点节点的深度
+		const std::string& label;	// 该节点在ggl::Graph的label
+
+		NodeInfo(int i, int pi, int d, const std::string& l = "n") :
+			index(i), postIndex(pi), depth(d), label(l){};
+	};
+
+	typedef std::pair<int, int> NodePair;
+
+private:
 	const sgm::Graph_boost<ggl::Graph>& m_GI;
 	GridMap m_gridMap;
-	std::queue<int> m_que;
-
+	std::queue<NodePair> m_que;
+	std::queue<NodeInfo> m_output;
 
 	void Step();
 public:
@@ -18,5 +31,5 @@ public:
 	CellularAutoma(const CellularAutoma& other) = delete;
 	CellularAutoma& operator = (const CellularAutoma& other) = delete;
 
-	void Generate();
+	void Generate(const std::string& beginLabel = "e");
 };
